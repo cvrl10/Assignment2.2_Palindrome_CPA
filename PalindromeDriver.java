42c88f9baa67c72ba90a2d1751739c2295717acd
Carl <cvrlix@gmail.com>
2022-01-23 21:32:31 -0500

    first commit

---
+++ PalindromeDriver.java
@@ -0,0 +1,13 @@
+
+/**
+ * @author Carl
+ * @version 1.0
+ * Assignment 2.2
+ */
+public class PalindromeDriver 
+{
+	public static void main(String[] args) 
+	{
+		Palindrome.main(args);
+	}//end main
+}//end class
